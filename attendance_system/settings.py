d67e63d9ba3527bd72a68e7a0be984d33e8956cd
XanakoneSPT <xs.xanakone@gmail.com>
2025-05-10 15:46:45 +0700

    modified many

--- attendance_system/settings.py
+++ attendance_system/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'attendance_db',
         'USER': 'postgres',
-        'PASSWORD': '123456',
+        'PASSWORD': '12345678',
         'HOST': 'localhost',
         'PORT': '5432',
     }
